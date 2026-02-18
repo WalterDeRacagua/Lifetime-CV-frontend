80a0e41b683e5eee0d38ce54343578fb7f130050
Sergio Carrasco <sesanc15ucm.es>
2026-02-18 09:54:05 +0100

    feat: configure PrimeNG v21 with new system

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
+
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    appConfig.providers,
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    }),
+  ],
+}).catch((err) => console.error(err));
